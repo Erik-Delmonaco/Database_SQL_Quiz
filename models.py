d92a6bd046ef008c0f025291acdeaefec41a3f2c
Erik-Delmonaco <delmonacoerik@gmail.com>
2026-03-05 01:34:26 +0000

    First Commit

---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlmodel import SQLModel, Field, create_engine
+class Bio(SQLModel, table=True):
+    name: str = Field(default=None, primary_key=True)
+    inducted: int | None = None
+    category: str | None = None
+    inducted_by: str | None = None
+
+class Inductees(SQLModel, table=True):
+    name: str = Field(default=None, primary_key=True)
+    class_year: int | None = None
+    category: str | None = None
+
+engine = create_engine("sqlite:///rockhall.db")
+SQLModel.metadata.create_all(engine)
